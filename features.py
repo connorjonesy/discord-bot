15de29200fe224bf4208f7d5ee9eca25f31c3a2f
Connor Jones <cja100@sfu.ca>
2026-03-07 17:32:23 -0800

    Fix: Issue #4 added split funcitonality to Blackjack

--- features.py
+++ features.py
@@ -1,11 +1,20 @@
 import random
 from asyncio import TimeoutError
 
-#TODO
-#TASK:: Double down and Splits
-
-
 class Blackjack:
+
+    CARD_EMOJIS = {
+        'A♠': '🂡', '2♠': '🂢', '3♠': '🂣', '4♠': '🂤', '5♠': '🂥', '6♠': '🂦', 
+        '7♠': '🂧', '8♠': '🂨', '9♠': '🂩', '10♠': '🂪', 'J♠': '🂫', 'Q♠': '🂭', 'K♠': '🂮',
+        'A♥': '🂱', '2♥': '🂲', '3♥': '🂳', '4♥': '🂴', '5♥': '🂵', '6♥': '🂶',
+        '7♥': '🂷', '8♥': '🂸', '9♥': '🂹', '10♥': '🂺', 'J♥': '🂻', 'Q♥': '🂽', 'K♥': '🂾',
+        'A♦': '🃁', '2♦': '🃂', '3♦': '🃃', '4♦': '🃄', '5♦': '🃅', '6♦': '🃆',
+        '7♦': '🃇', '8♦': '🃈', '9♦': '🃉', '10♦': '🃊', 'J♦': '🃋', 'Q♦': '🃍', 'K♦': '🃎',
+        'A♣': '🃑', '2♣': '🃒', '3♣': '🃓', '4♣': '🃔', '5♣': '🃕', '6♣': '🃖',
+        '7♣': '🃗', '8♣': '🃘', '9♣': '🃙', '10♣': '🃚', 'J♣': '🃛', 'Q♣': '🃝', 'K♣': '🃞',
+        'BACK': '🂠'
+    }
+
     def __init__(self, channel, player, bot):
         self.channel = channel
         self.player = player
@@ -15,12 +24,19 @@ class Blackjack:
         self.dealerScore = 0
         self.dealerHand = []
         self.playerHand = []
+        self.possibleSplit
+        self.playerSplit = []
+        self.splitScore = 0
 
     async def playgame(self):
         # Deal initial cards
         hand = self.generateHand()
-        dealerFirstCard = self.hit('Dealer')
-        await self.channel.send(f"Dealer reveals his first card: {dealerFirstCard}")
+        self.hit('Dealer', False)
+        await self.channel.send(f"Dealer reveals his first card: {self.dealerHand[0]}")
+        if self.playerHand[0] == self.playerHand[1]:
+            #split is possible
+            self.possibleSplit = True
+            await self.channel.send(f"{self.player.mention} gets a {hand[0]} and a {hand[1]}. Split, Hit or stay?")
         await self.channel.send(f"{self.player.mention} gets a {hand[0]} and a {hand[1]}. Hit or stay?")
 
         gameover = False
@@ -36,7 +52,7 @@ class Blackjack:
                 return
 
             if response.content.lower() in ['hit','hit me']:
-                newCard = self.hit('Player')
+                newCard = self.hit('Player', False)
                 await self.channel.send(f"You got a {newCard}!")
                 await self.channel.send(f"{self.player.mention}'s Score: {self.playerScore}")
                 if self.playerScore > 21:
@@ -45,11 +61,59 @@ class Blackjack:
                 else:
                     await self.channel.send(f"Hit or stay?")
 
+            elif response.content.lower() == 'split':
+                if self.possibleSplit != True:
+                    await self.channel.send(f"Invalid input: Try again, hit or stay?")
+                    continue
+                self.possibleSplit = False #just in case
+                self.playerSplit.append(self.playerHand.pop)
+                self.splitScores()
+                self.hit('Player', True)
+                #pick best hand
+                if self.playerScore < self.splitScore:
+                    self.playerScore = self.splitScore
+                    self.playerHand = self.playerSplit
+
+                """
+                TODO:: allow a splits loop for multiple hits and stays here
+                TODO:: create a seperate function/structure for dialogue
+                """
+                dealerBust = False
+
+                while self.dealerScore < 17:
+                    dealerCard = self.hit('Dealer', False)
+                    await self.channel.send(f"Dealer draws {dealerCard}")
+
+                if self.dealerScore > 21:
+                    dealerBust = True
+
+                dealer_cards = ", ".join(str(card) for card in self.dealerHand)
+                player_cards = ", ".join(str(card) for card in self.playerHand)
+
+                message = f"""
+                **Dealer's Hand:** {dealer_cards}
+                **Score:** {self.dealerScore}
+
+                **{self.player.display_name}'s Hand:** {player_cards}
+                **Score:** {self.playerScore}
+                """
+                await self.channel.send(message)
+                
+                if not dealerBust:
+                    if self.playerScore < self.dealerScore:
+                        await self.channel.send(f"{self.player.mention} berda_bot wins! You lose, hombre")
+                    elif self.playerScore == self.dealerScore:
+                        await self.channel.send(f"{self.player.mention} ties with the berda bot! Game over.")
+                    else:
+                        await self.channel.send(f"{self.player.mention} beats berda bot! You win!.")
+                    gameover = True
+
+
             elif response.content.lower() == 'stay':
                 dealerBust = False
 
                 while self.dealerScore < 17:
-                    dealerCard = self.hit('Dealer')
+                    dealerCard = self.hit('Dealer', False)
                     await self.channel.send(f"Dealer draws {dealerCard}")
 
                 if self.dealerScore > 21:
@@ -102,14 +166,29 @@ class Blackjack:
         hand = [card1,card2]
         return hand
 
-    def hit(self, caller):
+    def hit(self, caller, split):
         hand = None
-        card = random.randint(1,10)
-
         if caller == 'Player':
+            if split:
+                card = random.randint(1,10)
+                hand = self.playerSplit
+                self.splitScore += card
+                if card == 1:
+                    card =  'Ace'
+                if card == 10:
+                    card =  self.royals[random.randint(0,2)]
+                self.playerSplit.append(card)
+
+                if self.splitScore > 21:
+                    #check for Aces to change so as to not Bust
+                    for i in hand:
+                        if i == 'Ace':
+                            self.splitScore -= 10 #make the found Ace into a 1
+                            break
+                    
+            card = random.randint(1,10)
             hand = self.playerHand
             self.playerScore += card
-
             if card == 1:
                 card =  'Ace'
             if card == 10:
@@ -122,8 +201,10 @@ class Blackjack:
                     if i == 'Ace':
                         self.playerScore -= 10 #make the found Ace into a 1
                         break
+            return card
 
         elif caller == 'Dealer':
+            card = random.randint(1,10)
             hand = self.dealerHand
             self.dealerScore += card
 
@@ -139,5 +220,26 @@ class Blackjack:
                     if i == 'Ace':
                         self.dealerScore -= 10 #make the found Ace into a 1
                         break
+            return card
 
-        return card
+    def splitScores(self):
+        #error check
+        if self.splitScores == 0 or self.playerScore == 0:
+            print("Split Error")
+        #need to remove score from players OG hand and add score to split hand
+        #this means an extra check for cards that are ace or face
+        splitHand = self.playerSplit
+        if splitHand[0] == 'Ace':
+            self.splitScore += 11
+            self.playerScore -= 11
+        elif splitHand[0] in self.royals:
+            self.splitscore = 10
+            self.playerScore -= 10
+        else:
+            self.splitScore += splitHand[0]
+            self.playerScore -= splitHand[0]
+        return
+        
+
+
+
